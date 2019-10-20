5fcb5c853f9b158f140f9fb5ce5fda0b9c193c90
unknown <mayurxy08@gmail.com>
2019-10-20 10:05:50 -0700

    basic commit

---
+++ src/main/java/com/example/etheratest/EtheraTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.etheratest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtheraTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtheraTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/etheratest/EtheraTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.etheratest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtheraTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
